138fd268399f791fb74dcabe6f8e2c447d1e843f
GabrielNhoncanse <gabrielcnhoncanse@gmail.com>
2024-11-28 11:50:58 -0300

    Add Reflector class

--- src/lib/Reflector.ts
+++ src/lib/Reflector.ts
@@ -1,7 +1,26 @@
 interface Reflector {
   left: string[]
   right: string[]
-  reflect: (letter: string) => string
 }
 
+class Reflector implements Reflector {
+  left: string[]
+  right: string[]
 
+  constructor(mapping: string) {
+    this.left = mapping.slice(0, (mapping.length / 2)).split('')
+    this.right = mapping.slice(mapping.length / 2).split('')
+  }
+
+  reflect (letter: string): string {
+    const inLeft = this.left.indexOf(letter) !== 1
+
+    if (inLeft) {
+      return this.right[this.left.indexOf(letter)]
+    } else {
+      const inRight = this.right.indexOf(letter) !== 1
+
+      return inRight ? this.left[this.right.indexOf(letter)] : letter
+    }
+  }
+}
