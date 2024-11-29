b7a7c75aaf021393c8f4429e5a427b45ae71ca4c
Sarah <sarah.ribeiro@sou.inteli.edu.br>
2024-11-29 08:50:22 -0300

    new enigma logic

--- src/lib/Enigma.ts
+++ src/lib/Enigma.ts
@@ -1,72 +1,76 @@
-import { Plugboard } from "./Plugboard";
+import { Plugboard } from "./plugboard";
 import { Reflector } from "./Reflector";
 import Rotor from "./Rotor";
 
 class Enigma {
-  private rotors: Rotor[];
-  private plugboard: Plugboard;
-  private reflector: Reflector;
+    private reflector: Reflector;
+    private rotors: Rotor[];
+    private plugboard: Plugboard;
 
-  constructor(rotors: Rotor[], reflector: Reflector, plugboard: Plugboard) {
-    if (rotors.length !== 3) {
-      throw new Error("Enigma requires exactly 3 rotors.");
-    }
-    this.rotors = rotors;
-    this.reflector = reflector;
-    this.plugboard = plugboard;
-  }
-
-  setRotorPositions(positions: number[]): void {
-    if (positions.length !== this.rotors.length) {
-      throw new Error("Positions array must match the number of rotors.");
-    }
-    positions.forEach((pos, index) => {
-      this.rotors[index].rotate_to(pos);
-    });
-  }
-
-  private stepRotors(): void {
-    const rotateNext = this.rotors[0].rotate();
-
-    if (rotateNext) {
-      const rotateNext2 = this.rotors[1].rotate();
-
-      if (rotateNext2) {
-        this.rotors[2].rotate();
-      }
-    }
-  }
-
-  encipherLetter(letter: string): string {
-    if (!/[A-Z]/.test(letter)) return letter;
-
-    let signal = this.plugboard.forward(letter);
-
-    for (const rotor of this.rotors) {
-      signal = rotor.forward(signal);
+    constructor(reflector: Reflector, rotor1: Rotor, rotor2: Rotor, rotor3: Rotor, plugboard: Plugboard) {
+        this.reflector = reflector;
+        this.rotors = [rotor1, rotor2, rotor3];
+        this.plugboard = plugboard;
     }
 
-    signal = this.reflector.reflect(signal);
-
-    for (const rotor of [...this.rotors].reverse()) {
-      signal = rotor.backward(signal);
+    setRotors(positions: number[]): void {
+        this.rotors.forEach((rotor, i) => {
+            rotor.position = positions[i];
+        });
     }
 
-    signal = this.plugboard.backward(signal);
+    encipher(char: string): string {
+        if (!/[A-Z]/.test(char)) return char;
 
-    return signal;
-  }
+        // Passa pelo plugboard
+        char = this.plugboard.forward(char);
 
-  processMessage(message: string): string {
-    return message
-      .toUpperCase()
-      .split("")
-      .map((char) => {
-        this.stepRotors();
-        return this.encipherLetter(char);
-      })
-      .join("");
-  }
+        // Passa pelos rotores (ida)
+        this.rotors.forEach(rotor => {
+            char = rotor.forward(char);
+        });
+
+        // Passa pelo refletor
+        char = this.reflector.reflect(char);
+
+        // Passa pelos rotores (volta)
+        this.rotors.slice().reverse().forEach(rotor => {
+            char = rotor.backward(char);
+        });
+
+        // Passa novamente pelo plugboard
+        char = this.plugboard.backward(char);
+
+        // Roda o primeiro rotor
+        if (this.rotors[0].rotate()) {
+            if (this.rotors[1].rotate()) {
+                this.rotors[2].rotate();
+            }
+        }
+
+        return char;
+    }
+
+    processMessage(message: string): string {
+        return message.split('').map(char => this.encipher(char)).join('');
+    }
 }
 
-export default Enigma;
+// const I   = new Rotor('EKMFLGDQVZNTOWYHXUSPAIBRCJ','Q')
+// const II  = new Rotor('AJDKSIRUXBLHWTMCQGZNPYFVOE', 'E')
+// const III = new Rotor('BDFHJLCPRTXVZNYEIWGAKMUSQO', 'V')
+// const IV  = new Rotor('ESOVPZJAYQUIRHXLNFTGKDCMWB', 'J')
+// const V  = new Rotor('VZBRGITYUPSDNHLXAWMJQOFECK', 'Z')
+
+
+// const B = new Reflector('YRUHQSLDPXNGOKMIEBFZCWVJAT')
+// const C = new Reflector('FVPJIAOYEDRZXWGCTKUQSBNMHL')
+// const pb = new Plugboard(['AR', 'GK', 'OX'])
+// const enigma = new Enigma(B, I, II, III, pb)
+// enigma.setRotors([1, 2, 3])
+// const original = 'TESTANDO'
+// let criptografado = ''
+
+// for (let c of original){
+//    criptografado += enigma.encipher(c)
+// }
